9993339cbb7d36afbdd3859d08cd1b1e675a3d4d
Panda64 <JaylenSchelb@yahoo.com>
2022-03-03 01:51:33 -0800

    Completed scraping portion

--- scrape.go
+++ scrape.go
@@ -3,21 +3,38 @@ package main
 import (
 	"fmt"
 
+	"strconv"
+
 	"github.com/gocolly/colly"
 )
 
-// main() contains code adapted from example found in Colly's docs:
-// http://go-colly.org/docs/examples/basic/
+type Games struct {
+	current_players string
+	peak_today      string
+	game            string
+	game_link       string
+}
+
 func main() {
+
+	var games = []Games{}
+
 	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+	c.OnHTML("#detailStats > table > tbody", func(e *colly.HTMLElement) {
 
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		for count := 3; count < 12; count++ {
+			games = append(games, Games{
+				current_players: e.ChildText("tr:nth-child(" + strconv.Itoa(count) + ") > td:nth-child(1)"),
+				peak_today:      e.ChildText("tr:nth-child(" + strconv.Itoa(count) + ") > td:nth-child(2)"),
+				game:            e.ChildText("tr:nth-child(" + strconv.Itoa(count) + ") > td:nth-child(4)"),
+				game_link:       e.ChildAttr("tr:nth-child("+strconv.Itoa(count)+") > td:nth-child(4) > a", "href"),
+			})
+		}
+
+		// Print games
+		fmt.Println(games)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -25,6 +42,11 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	// If something goes wrong, print the error
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Something went wrong:", err)
+	})
+
+	// Visit the steam statistics page
+	c.Visit("https://store.steampowered.com/stats/Steam-Game-and-Player-Statistics")
 }
