b3c34b640b1f7ca9c058b3e77c1704d2b1780672
Panda64 <JaylenSchelb@yahoo.com>
2022-03-03 02:10:38 -0800

    Seralize data into JSON file

--- scrape.go
+++ scrape.go
@@ -1,18 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-
+	"io/ioutil"
 	"strconv"
 
 	"github.com/gocolly/colly"
 )
 
 type Games struct {
-	current_players string
-	peak_today      string
-	game            string
-	game_link       string
+	Game           string `json:"game"`
+	CurrentPlayers string `json:"current_players"`
+	PeakToday      string `json:"peak_today"`
+	GameLink       string `json:"game_link"`
 }
 
 func main() {
@@ -22,24 +23,24 @@ func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
 
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
 	c.OnHTML("#detailStats > table > tbody", func(e *colly.HTMLElement) {
 
 		for count := 3; count < 12; count++ {
 			games = append(games, Games{
-				current_players: e.ChildText("tr:nth-child(" + strconv.Itoa(count) + ") > td:nth-child(1)"),
-				peak_today:      e.ChildText("tr:nth-child(" + strconv.Itoa(count) + ") > td:nth-child(2)"),
-				game:            e.ChildText("tr:nth-child(" + strconv.Itoa(count) + ") > td:nth-child(4)"),
-				game_link:       e.ChildAttr("tr:nth-child("+strconv.Itoa(count)+") > td:nth-child(4) > a", "href"),
+				Game:           e.ChildText("tr:nth-child(" + strconv.Itoa(count) + ") > td:nth-child(4)"),
+				CurrentPlayers: e.ChildText("tr:nth-child(" + strconv.Itoa(count) + ") > td:nth-child(1)"),
+				PeakToday:      e.ChildText("tr:nth-child(" + strconv.Itoa(count) + ") > td:nth-child(2)"),
+				GameLink:       e.ChildAttr("tr:nth-child("+strconv.Itoa(count)+") > td:nth-child(4) > a", "href"),
 			})
 		}
 
-		// Print games
-		fmt.Println(games)
-	})
-
-	// Before making a request print "Visiting ..."
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
+		file, _ := json.MarshalIndent(games, "", " ")
+		_ = ioutil.WriteFile("game_stats.json", file, 0644)
 	})
 
 	// If something goes wrong, print the error
